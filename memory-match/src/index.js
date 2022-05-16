7046eb40b138e55b6d5588d5513d7e528cd66bca
Isabelle Goralsky <isabellegoralsky@Isabelles-Air.lan>
2022-05-16 16:16:30 -0700

    first commit for header implementation: visuals have been added. functionality + specific visual details are yet to be implemented

--- memory-match/src/index.js
+++ memory-match/src/index.js
@@ -1,3 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+
+class NavBar extends React.Component {
+  render () {
+    return (
+      <nav>
+        <ul class="nav-items">
+          <li><a class="nav-item" href="google.com">GAME</a></li>
+          <li><a class="nav-item" href="google.com">COMMENTS/CONCERNS</a></li>
+          <li><a class="nav-item" href="google.com">LIBRARY</a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<Library />);
+root.render(<NavBar />);
+
+
