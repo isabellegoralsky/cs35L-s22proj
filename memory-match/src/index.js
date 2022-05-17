fe16c1e837b28ee5646afa59f5032dccea57976f
Isabelle Goralsky <isabellegoralsky@Isabelles-Air.lan>
2022-05-16 21:29:58 -0700

    basic skeleton/frame of library page is implemention. React component names / framework made. Navbar still in the works. Board/Square code taken from CS35L assign4 for temp use.

--- memory-match/src/index.js
+++ memory-match/src/index.js
@@ -2,6 +2,101 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+function Square(props){
+  return (
+    <button
+      className="square" 
+      onClick={()=> props.onClick()}>
+      {props.value}
+    </button>
+  );
+}
+
+class Library extends React.Component {
+  constructor(props) {
+    super(props);
+    let value_range = [];
+    for (var i = 0; i <= 31; i++) {   
+      value_range.push(i);
+    }
+    this.state = {
+      square_show: Array(64).fill(null),
+      square_matched: Array(64).fill(null),
+      square_value: value_range,
+      first_pick: true,
+      first_pick_index: -1,
+    };
+  }
+
+  handleClick(i) {
+    return;
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.square_show[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    let status = "title TBD"
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(32)}
+          {this.renderSquare(33)}
+          {this.renderSquare(34)}
+          {this.renderSquare(35)}
+          {this.renderSquare(36)}
+          {this.renderSquare(37)}
+          {this.renderSquare(38)}
+          {this.renderSquare(39)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class LPage extends React.Component {
+  render() {
+    return (
+      <div>
+      <div className="navigation-div"> <NavBar /> </div>
+      <div className="libraries">
+        <div className="library-titles">
+          <div>LIB1</div>
+        </div>
+        <div className="library">
+          <Library />
+        </div>
+        <div className="library-titles">
+          <div>LIB2</div>
+        </div>
+        <div className="library">
+          <Library />
+        </div>
+        <div className="library-titles">
+          <div>LIB3</div>
+        </div>
+        <div className="library">
+          <Library />
+        </div>
+        <div className="library-titles">
+          <div>LIB4</div>
+        </div>
+        <div className="library">
+          <Library />
+        </div>
+      </div>
+      </div>
+    );
+  }
+}
+
 class NavBar extends React.Component {
   render () {
     return (
@@ -20,7 +115,6 @@ class NavBar extends React.Component {
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(<Library />);
-root.render(<NavBar />);
+root.render(<LPage />);
 
 
