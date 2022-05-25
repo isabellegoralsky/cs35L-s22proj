9b419f2a8b4f6154be26c5a22af24abce4519d48
Isabelle Goralsky <isabellegoralsky@Isabelles-Air.lan>
2022-05-25 15:58:09 -0700

    finished page styling + header + footer. todo: add functionality to switch between themes of the library. currently, buttons do not work. also todo: add more images up to 8x8 grid.

--- memory-match/src/index.js
+++ memory-match/src/index.js
@@ -5,61 +5,76 @@ import './index.css';
 function Square(props){
   return (
     <button
-      className="square" 
+      className="options" 
       onClick={()=> props.onClick()}>
       {props.value}
     </button>
   );
 }
 
-class Library extends React.Component {
-  constructor(props) {
-    super(props);
-    let value_range = [];
-    for (var i = 0; i <= 31; i++) {   
-      value_range.push(i);
-    }
-    this.state = {
-      square_show: Array(64).fill(null),
-      square_matched: Array(64).fill(null),
-      square_value: value_range,
-      first_pick: true,
-      first_pick_index: -1,
-    };
-  }
+function Image(props){
+  return (
+    <img className="libimages" src={props.source} alt="library" width="200" height="200"/>
+  )
+}
 
-  handleClick(i) {
+  function handleClick(i) {
     return;
   }
 
-  renderSquare(i) {
+  function renderSquare(i) {
     return (
       <Square
-        value={this.state.square_show[i]}
+        value={i}
         onClick={() => this.handleClick(i)}
       />
     );
   }
 
-  render() {
-    let status = "title TBD"
+  function renderImage(i) {
+    return (
+      <Image
+        source={i}
+      />
+    );
+  }
+
+  function Library() {
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(32)}
-          {this.renderSquare(33)}
-          {this.renderSquare(34)}
-          {this.renderSquare(35)}
-          {this.renderSquare(36)}
-          {this.renderSquare(37)}
-          {this.renderSquare(38)}
-          {this.renderSquare(39)}
+        <div className="status">Browse the 4 library previews below to customize your playing cards on the main page!</div>
+        <div className="links">
+          <div className="lib">{renderSquare("Puppy Theme")}</div>
+          <div className="lib">{renderSquare("Baby Chickens Theme")}</div>
+          <div className="lib">{renderSquare("Bulldog Theme")}</div>
+          <div className="lib">{renderSquare("Sea Life Theme")}</div>
+        </div>
+        <div className="rows">
+          <div className="img-row">
+            <div className="lib-img">{renderImage("bulldog/bull1.jpg")}</div>
+            <div className="lib-img">{renderImage("bulldog/bull2.jpg")}</div>
+            <div className="lib-img">{renderImage("bulldog/bull3.jpg")}</div>
+          </div>
+          <div className="img-row">
+            <div className="lib-img">{renderImage("bulldog/bull4.jpg")}</div>
+            <div className="lib-img">{renderImage("bulldog/bull5.JPG")}</div>
+            <div className="lib-img">{renderImage("bulldog/bull6.jpg")}</div>
+          </div>
+          <div className="img-row">
+            <div className="lib-img">{renderImage("bulldog/bull7.jpg")}</div>
+            <div className="lib-img">{renderImage("bulldog/bull8.jpg")}</div>
+            <div className="lib-img">{renderImage("bulldog/bull9.jpg")}</div>
+          </div>
         </div>
       </div>
     );
   }
-}
+
+  function renderLib(){
+    return (
+      <Library />
+    );
+  }
 
 class LPage extends React.Component {
   render() {
@@ -67,31 +82,14 @@ class LPage extends React.Component {
       <div>
       <div className="navigation-div"> <NavBar /> </div>
       <div className="libraries">
-        <div className="library-titles">
-          <div>LIB1</div>
+        <div>
+          <div className="library-titles">GAME CARD LIBRARY</div>
         </div>
         <div className="library">
-          <Library />
-        </div>
-        <div className="library-titles">
-          <div>LIB2</div>
-        </div>
-        <div className="library">
-          <Library />
-        </div>
-        <div className="library-titles">
-          <div>LIB3</div>
-        </div>
-        <div className="library">
-          <Library />
-        </div>
-        <div className="library-titles">
-          <div>LIB4</div>
-        </div>
-        <div className="library">
-          <Library />
+          {renderLib()}
         </div>
       </div>
+      <div className="navigation-div"> <Foot /> </div>
       </div>
     );
   }
@@ -107,6 +105,19 @@ class NavBar extends React.Component {
           <li><a class="nav-item" href="google.com">LIBRARY</a></li>
         </ul>
       </nav>
+    )
+  }
+}
+
+class Foot extends React.Component {
+  render () {
+    return (
+      <footer>
+        <div className="foot">
+          made by md, ig, sr<br/>
+          cs35L spr 2022
+        </div>
+      </footer>
     )
   }
 }
